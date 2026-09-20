2d24487d5397a62d201576c3aeb64a5d5c6c93c3
okafor victoria109 <victoriachidubem31@gmail.com>
2026-09-20 20:11:05 +0100

    Update stage2_virtual_atm.py

--- stage2_virtual_atm.py
+++ stage2_virtual_atm.py
@@ -1,1 +1,17 @@
-
+balance = 20000
+while True:
+  user_option = input("Choose an option (balance, withdraw, exit): ").lower().strip()
+  if user_option == "balance":
+    print(f"Your current balance is: #{balance}")
+  elif user_option == "exit":
+    print("Goodbye Thanks for using our ATM, have a wonderful day")
+    break
+  elif user_option == "withdraw":
+    amount = int(input("Enter the amount you want to withdraw: "))
+    if amount <= balance:
+      balance = balance - amount
+      print(f"Withdrawal successful! you withdrew: #{amount}")
+    else:
+      print("Insufficient funds!")
+else:
+  print("Invalid option! please choose balance, withdraw, or exit.")
