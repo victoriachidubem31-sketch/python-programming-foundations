d6cdf9b58b146b5456beae60177bceb2f42a78e9
okafor victoria109 <victoriachidubem31@gmail.com>
2026-09-20 20:13:36 +0100

    Update stage4_grade_ledger.py

--- stage4_grade_ledger.py
+++ stage4_grade_ledger.py
@@ -1,1 +1,15 @@
+grade_book = {}
+while True:
+  user_choice = input("Pick an option (add, view or exit): ")
+  if user_choice == "add":
+   course = input("Enter course code (e.g., MTH101): ").upper().strip()
+   score = int(input("Enter the score (0-100): "))
+   grade_book[course] = score
+   print(f"Added {course} with a score of {score}")
+  elif user_choice == "view":
+   print(grade_book)
+  elif user_choice == "exit":
+   break
+  else:
+    print("Invalid choice pick add, view or exit")
 
